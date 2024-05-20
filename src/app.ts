0fecb2ade5019f85907520e2f2cf0bb0f175e77d
Kabir <kabirshuvo19@gmail.com>
2024-05-21 04:49:25 +0600

    Initial setup done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 // const port = 3000;
@@ -9,8 +9,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
