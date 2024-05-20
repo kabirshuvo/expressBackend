fe43991b8df50ea71f94659132b42d13bdfb872c
Kabir <kabirshuvo19@gmail.com>
2024-05-21 04:12:52 +0600

    Project setup ongoing

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port = 3000;
+// const port = 3000;
 
 //parsers
 
