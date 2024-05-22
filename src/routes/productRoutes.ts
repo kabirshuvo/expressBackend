b137fc2acde71bd09e3ad4edf6840a654ef9f639
Kabir <kabirshuvo19@gmail.com>
2024-05-22 17:55:44 +0600

    Now getting the products in post man

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import ProductController from '../controllers/ProductController';
+import OrderController from '../controllers/OrderController';
+
 const router = express.Router();
 
 router.post('/create-product', ProductController.createProduct);
@@ -9,4 +11,11 @@ router.delete('/create-product/:id', ProductController.deleteProduct);
 router.get('/all-products', ProductController.getAllProducts);
 router.get('/search', ProductController.searchProducts);
 
+// order routes
+
+router.post('/', OrderController.createOrder);
+router.get('/', OrderController.getAllOrders);
+router.get('/order/:id', OrderController.getOrder);
+router.put('/order/:id', OrderController.updateOrder);
+router.delete('/order/:id', OrderController.deleteOrder);
 export default router;
